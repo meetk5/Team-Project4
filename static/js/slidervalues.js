1e3929a0e92c832622d0e5a9085d8b51e1e178a5
Saleha <salehaahmed456@gmail.com>
2021-11-04 20:37:12 -0400

    value outputs working for all sliders

--- static/js/slidervalues.js
+++ static/js/slidervalues.js
@@ -1,7 +1,58 @@
-var slider = document.getElementById("myHappyRange");
-var output = document.getElementById("outputHappy");
-output.innerHTML = slider.value;
+var slider_good = document.getElementById("myGoodRange");
+var output_good = document.getElementById("outputGood");
+output_good.innerHTML = slider_good.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
+slider_good.oninput = function() {
+  output_good.innerHTML = this.value;
 }
+
+
+
+
+var slider_fun = document.getElementById("myFunRange");
+var output_fun = document.getElementById("outputFun");
+output_fun.innerHTML = slider_fun.value;
+
+slider_fun.oninput = function() {
+  output_fun.innerHTML = this.value;
+}
+
+
+
+var slider_wow = document.getElementById("myWowRange");
+var output_wow = document.getElementById("outputWow");
+output_wow.innerHTML = slider_wow.value;
+
+slider_wow.oninput = function() {
+  output_wow.innerHTML = this.value;
+}
+
+
+
+var slider_sad = document.getElementById("mySadRange");
+var output_sad = document.getElementById("outputSad");
+output_sad.innerHTML = slider_sad.value;
+
+slider_sad.oninput = function() {
+  output_sad.innerHTML = this.value;
+}
+
+
+
+var slider_soso = document.getElementById("mySosoRange");
+var output_soso = document.getElementById("outputSoso");
+output_soso.innerHTML = slider_soso.value;
+
+slider_soso.oninput = function() {
+  output_soso.innerHTML = this.value;
+}
+
+
+var slider_bad = document.getElementById("myBadRange");
+var output_bad = document.getElementById("outputBad");
+output_bad.innerHTML = slider_bad.value;
+
+slider_bad.oninput = function() {
+  output_bad.innerHTML = this.value;
+}
+
