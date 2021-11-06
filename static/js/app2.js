6bf5d5918ba616545eea4b64679312b4a8daf3e5
Bjohnson08021 <84213057+Bjohnson08021@users.noreply.github.com>
2021-11-06 14:24:32 -0400

    created js files, made some jason files, working on network runtime dropdown,

---
+++ static/js/app2.js
@@ -0,0 +1,210 @@
+
+//function init() {
+  
+    function dropdown(network) {
+      d3.json("show_display.json").then((data) => {
+        // //get samples info from json file
+        //console.log("data", data)
+        let shows = [];
+        for (var i = 0; i < data.length; i++) {
+          shows.push(data[i]);
+        }
+        console.log(shows)
+        let display = d3.select("#sample-metadata2");
+        display.html("");
+        // filter to get different boro's cuisines
+        // const twoCategories = { data: shows.map(({ network, name }) => ({ network, name })) };
+        // console.log("twoLabels", twoCategories)
+    
+        let networkSamples = data.filter(function (citation) {
+          return citation.name == network
+        });
+        console.log(networkSamples)
+        // let episodeSamples = networkSamples.filter(function (episodes) {
+        //   return episodes.name == followers
+        // });
+        // console.log(episodeSamples)
+        let networkCounts = getcounts(networkSamples, "follower");
+        console.log("networkCounts", networkCounts)
+        console.log(Object.keys(networkCounts))
+        console.log(Object.values(networkCounts));
+        // let followerCounts = getcounts(networkSamples, "followers");
+        // console.log("followers", followerCounts)
+    
+        let networkSampleResults = Object.keys(networkCounts).map(f => ({ type: f, count: networkCounts[f] }))
+        console.log("networkSampleResults", networkSampleResults)
+        // Sort the data by the most number of violations in each boro in descending (most to least)
+        let netValues = Object.values(networkSampleResults).sort((a, b) => b.count - a.count);
+        console.log("netvalues", netValues)
+        // Slice the first 5 objects
+        SlicedData = netValues.slice(0, 5);
+        console.log("SlicedData", SlicedData)
+    
+        // let followerResults = Object.keys(followerCounts).map(f => ({ type: f, count: followerCounts[f] }))
+        // console.log("followerResults", followerResults)
+        // // Sort the data by the most number of violations in each boro in descending (most to least)
+        // let followerValues = Object.values(followerResults).sort((a, b) => b.count - a.count);
+        // console.log("followerValues", followerValues)
+        // // Slice the first 5 objects
+        // followerSlicedData = followerValues.slice(0, 5);
+        // console.log("followerSlicedData", followerSlicedData)
+        // //  //get a list of the 5 top violations
+        episodeDisplay = []
+        for (var s = 0; s < SlicedData.length; s++) {
+          episodeDisplay.push(SlicedData[s].type);
+        }
+        console.log("episodedisplay", episodeDisplay)
+        //loop to get violations info into the top violations in each boro box
+        Object.entries(episodeDisplay).forEach(([, b]) => {
+          display.append("h5").text(`- ${b}`);
+        });
+    
+        // let dropDown = d3.select("#selDataset");
+        // network.forEach((c) => {
+        //   dropDown.append("option").text(c).property("value", c)
+        // });
+        // let dropDown2 = d3.select("#selDataset2");
+        // tvList.forEach((o) => {
+        //   dropDown2.append("option").text(o).property("value", o)
+        // })
+      })
+    }
+    
+    dropdown("55")
+    
+    
+    // function dropdown2(network) {
+    //   d3.json("show_display.json").then((shows) => {
+    //     // //get samples info from json file
+    //     //console.log("data", data)
+    //     // let shows = [];
+    //     // for (var i = 0; i < data.length; i++) {
+    //     //   shows.push(data[i]);
+    //     // }
+    //     let display = d3.select("#sample-metadata2");
+    //     display.html("");
+    //     //console.log(data)
+    //     console.log(network)
+    //     console.log(shows)
+    //     let runTimeSamples = shows.filter(sampleobject => sampleobject.network == network);
+    //     // let runTimeSamples = shows.filter(function (runTime) {
+    //     //   return runTime.network == network
+    //     // });
+    //     console.log(runTimeSamples)
+    //     // let episodeSamples = networkSamples.filter(function (episodes) {
+    //     //   return episodes.name == followers
+    //     // });
+    //     // console.log(episodeSamples)
+    //     let runTimeCounts = getcounts(runTimeSamples, "name");
+    //     console.log("networkCounts", runTimeCounts)
+    //     console.log(Object.keys(runTimeCounts))
+    //     console.log(Object.values(runTimeCounts));
+    //     let runTimeSampleResults = Object.keys(runTimeCounts).map(f => ({ type: f, count: runTimeCounts[f] }))
+    //     console.log("runTimeResults", runTimeSampleResults)
+    //     // Sort the data by the most number of violations in each boro in descending (most to least)
+    //     let runTimeValues = Object.values(runTimeSampleResults).sort((a, b) => b.count - a.count);
+    //     console.log("runTimevalues", runTimeValues)
+    //     // Slice the first 5 objects
+    //     runTimeSlicedData = runTimeValues.slice(0, 5);
+    //     console.log("runtimeSlicedData", runTimeSlicedData)
+    //     runTimeDisplay = []
+    //     for (var s = 0; s < runTimeSlicedData.length; s++) {
+    //       runTimeDisplay.push(runTimeSlicedData[s].type);
+    //     }
+    //     console.log("runtimedisplay", runTimeDisplay)
+    //     //loop to get violations info into the top violations in each boro box
+    //     Object.entries(runTimeDisplay).forEach(([, b]) => {
+    //       display.append("h5").text(`- ${b}`);
+    //     });
+    //     // dropdown2("75")
+    
+    //   })
+    // }
+    //changes with dropdown changes
+    function optionChanged(view) {
+      dropdown(view);
+     // dropdown2(view)
+    
+    
+    };
+    
+    //initializes everything when the page runs
+    function init() {
+    
+    
+      // //  // see dropdown
+      d3.json("show_display.json").then((data) => {
+        console.log(data)
+        let shows = [];
+        for (var i = 0; i < data.length; i++) {
+          shows.push(Object.values(data)[i]);
+        }
+        console.log("shows", shows)
+        var obj = {};
+        shows.forEach(function (value) {
+          //console.log(value)
+          if (obj[value["network"]]) {
+            obj[value["network"]]++;
+          }
+          else {
+            obj[value["network"]] = 1;
+          }
+        })
+        console.log(obj)
+        console.log("ID#", shows)
+        const twoLabels = { data: shows.map(({ network, name }) => ({ network, name })) };
+        console.log("twoLabels", twoLabels)
+        let results = Object.keys(obj).map(e => ({ type: e, count: obj[e] }))
+        console.log("results", results)
+        network = []
+        for (var j = 0; j < results.length; j++) {
+          network.push(results[j].type);
+        }
+        console.log("network", network)
+        var tvShows = {};
+        shows.forEach(function (value) {
+          //console.log(value)
+          if (tvShows[value["runtime"]]) {
+            tvShows[value["runtime"]]++;
+          }
+          else {
+            tvShows[value["runtime"]] = 1;
+          }
+        })
+        console.log(tvShows)
+        let tvResults = Object.keys(tvShows).map(e => ({ type: e, count: obj[e] }))
+        console.log("tvResults", tvResults)
+        tvList = []
+        for (var j = 0; j < tvResults.length; j++) {
+          tvList.push(tvResults[j].type);
+        }
+    
+        // let dropDown = d3.select("#selDataset");
+        // network.forEach((c) => {
+        //   dropDown.append("option").text(c).property("value", c)
+        // });
+        let dropDown2 = d3.select("#selDataset2");
+        tvList.forEach((o) => {
+          dropDown2.append("option").text(o).property("value", o)
+        })
+      })
+    }
+    
+    //setting function to get any 2 different elements of the json file
+    function getcounts(dataArray, myParam) {
+    
+      var finalArray = {};
+      dataArray.forEach(function (networkNames) {
+        //console.log(value)
+        if (finalArray[networkNames[myParam]]) {
+          finalArray[networkNames[myParam]]++;
+        }
+        else {
+          finalArray[networkNames[myParam]] = 1;
+        }
+      });
+      return finalArray;
+    
+    }
+    //}
+    init()
