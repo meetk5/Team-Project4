85aaf60855b8feb547f108bf7601e19317ee540e
Bjohnson08021 <84213057+Bjohnson08021@users.noreply.github.com>
2021-11-08 19:31:49 -0500

    trying d3 method for slider

--- static/js/home.js
+++ static/js/home.js
@@ -15,6 +15,34 @@
 //         return network[runtime].some(filter => shows.some(v => v.network === filter));
 //         }
 //       });
+
+
+// var sliderSimple = d3
+// //.sliderBottom()
+// //.min(d3.min(data))
+// //.max(d3.max(data))
+// //.width(300)
+// //.tickFormat(d3.format('.2%'))
+// //.ticks(5)
+// //.default(0.015)
+// .on('onchange', val => {
+//   d3.select('p#value-simple').text(d3.format('.2%')(val));
+// });
+
+// var gSimple = d3
+// .select('div#slider-simple')
+// .append('svg')
+// .attr('width', 500)
+// .attr('height', 100)
+// .append('g')
+// .attr('transform', 'translate(30,30)');
+
+// gSimple.call(sliderSimple);
+
+
+
+
+
 var slider_good = document.getElementById("myGoodRange");
 var output_good = document.getElementById("outputGood");
 output_good.innerHTML = slider_good.value;
