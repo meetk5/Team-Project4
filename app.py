5bd869d320e3486430c3b19353ffdaa87f0a11ee
JayD <84251992+jaybdhruv@users.noreply.github.com>
2021-11-09 17:36:15 -0500

    database integration with flask

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import config
 import requests
 import json
 from flask import Flask, render_template, request, jsonify
 import joblib
 from sklearn.preprocessing import LabelEncoder
+
+# Database Setup
+database = 'tvtimedb'
+engine = create_engine(f'postgresql://postgres:{config.password}@localhost:{config.port}/{database}')
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+tv_info = Base.classes.tvshows
 
 app = Flask(__name__)
 
@@ -16,18 +32,45 @@ def network():
     print("rendering network page")
     return render_template("network.html")
 
+@app.route("/team")
+def team():
+    print("rendering team page")
+    return render_template("team.html")
+
+@app.route("/followers")
+def followers():
+    print("rendering follwers page")
+    return render_template("followers.html")
+
+@app.route("/visualizations")
+def visualizations():
+    print("rendering visualizations page")
+    return render_template("vis1.html")
+
+
 @app.route("/get_your_recommendation", methods=["POST"])
 def get_your_recommendation():
 
     print("starting recommendation page")
-    feature_list = request.get_json()
-    print(f"Features: {feature_list}")
 
-    moodlist = []
-    for mood in feature_list:
-        moodlist.append(mood)
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all passengers
+    results = session.query(tv_info.network).distinct().all()
+    session.close()
+
+    print(f"Networks: {results}")
+
+    moodlist = request.get_json()
+    print(f"Features/Moods: {moodlist}")
+
+    # moodlist = []
     
-    print(f"Moods: {moodlist}")
+    # for mood in feature_list:
+    #     moodlist.append(mood)
+    
+    # print(f"Moods: {moodlist}")
 
     filename = "network_predictor.h5"
     network_predictor_model = joblib.load(filename)
@@ -35,20 +78,17 @@ def get_your_recommendation():
 
     le = LabelEncoder()
 
+    y_network = le.fit_transform(results)
+    print(f"Network Labels: {y_network}")
+
     result = network_predictor_model.predict([moodlist])
     print(f"Result: {result}")
 
     suggested_network = le.inverse_transform(result)
 
-    print(f"Network: {suggested_network}")
+    print(f"Predicted Network: {suggested_network}")
 
     return jsonify(suggested_network[0])
-
-
-
-
-
-
 
 
 
--- static/js/network_predictor.js
+++ static/js/network_predictor.js
@@ -1,112 +1,126 @@
 var form = d3.select("form");
+form.on("#submitbtn", findnetwork);
+form.on(type="reset", clearForm);
 
-form.on("submit", findnetwork);
+// var form = d3.select("form");
+// form.on("submit", findnetwork);
+// form.on("reset", clearForm);
 
-form.on("reset", clearForm);
+// Good Mood input
+var slider_good = document.getElementById("myGoodRange");
+var output_good = document.getElementById("outputGood");
+output_good.innerHTML = slider_good.value;
 
-var good = d3.select("#myGoodRange").property("value");
-
-console.log(good)
-
-
-// var slider_good = document.getElementById("myGoodRange");
-// var output_good = document.getElementById("outputGood");
-// output_good.innerHTML = slider_good.value;
-
-
-// slider_good.onchange = function () {
-//     output_good.innerHTML = this.value;
-//     console.log(output_good.innerHTML)
-// }
-
-// var good = d3.select("#myGoodRange").property("value");
-// console.log(good);
-
-// console.log(slider_good)
-// console.log(output_good)
-
-
-
-// var slider_fun = document.getElementById("myFunRange");
-// var output_fun = document.getElementById("outputFun");
-// output_fun.innerHTML = slider_fun.value;
-
-// slider_fun.oninput = function () {
-//     output_fun.innerHTML = this.value;
-// }
-
-
-
-// var slider_wow = document.getElementById("myWowRange");
-// var output_wow = document.getElementById("outputWow");
-// output_wow.innerHTML = slider_wow.value;
-
-// slider_wow.oninput = function () {
-//     output_wow.innerHTML = this.value;
-// }
-
-
-
-// var slider_sad = document.getElementById("mySadRange");
-// var output_sad = document.getElementById("outputSad");
-// output_sad.innerHTML = slider_sad.value;
-
-// slider_sad.oninput = function () {
-//     output_sad.innerHTML = this.value;
-// }
-
-
-
-// var slider_soso = document.getElementById("mySosoRange");
-// var output_soso = document.getElementById("outputSoso");
-// output_soso.innerHTML = slider_soso.value;
-
-// slider_soso.oninput = function () {
-//     output_soso.innerHTML = this.value;
-// }
-
-
-// var slider_bad = document.getElementById("myBadRange");
-// var output_bad = document.getElementById("outputBad");
-// output_bad.innerHTML = slider_bad.value;
-
-// slider_bad.oninput = function () {
-//     output_bad.innerHTML = this.value;
-// }
-
-function findnetwork() {
-    // d3.event.preventDefault();
-
-    var good = d3.select("#myGoodRange").property("value");
-    var fun = d3.select("#myFunRange").property("value");
-    var wow = d3.select("#myWowRange").property("value");
-    var sad = d3.select("#mySadRange").property("value");
-    var soso = d3.select("#mySosoRange").property("value");
-    var bad = d3.select("#myBadRange").property("value");
-    console.log(good);
-
-    var moodList = [good, fun, wow, sad, soso, bad];
-    console.log(moodList)
-
-    d3.json("/get_your_recommendation", {
-        method: "POST",
-        body: JSON.stringify(
-            moodList
-        ),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
+slider_good.oninput = function() {
+  output_good.innerHTML = this.value;
 }
 
+var good = d3.select("#myGoodRange").property("value");
+console.log(good);
 
+// Fun Mood Input
+var slider_fun = document.getElementById("myFunRange");
+var output_fun = document.getElementById("outputFun");
+output_fun.innerHTML = slider_fun.value;
+
+slider_fun.oninput = function() {
+  output_fun.innerHTML = this.value;
+}
+
+var fun = d3.select("#myFunRange").property("value");
+console.log(fun);
+
+// Wow Mood Input
+var slider_wow = document.getElementById("myWowRange");
+var output_wow = document.getElementById("outputWow");
+output_wow.innerHTML = slider_wow.value;
+
+slider_wow.oninput = function() {
+  output_wow.innerHTML = this.value;
+}
+
+var wow = d3.select("#myWowRange").property("value");
+console.log(wow);
+
+// Sad Mood Input
+var slider_sad = document.getElementById("mySadRange");
+var output_sad = document.getElementById("outputSad");
+output_sad.innerHTML = slider_sad.value;
+
+slider_sad.oninput = function() {
+  output_sad.innerHTML = this.value;
+}
+
+var sad = d3.select("#mySadRange").property("value");
+console.log(wow);
+
+// So-So Mood Input
+var slider_soso = document.getElementById("mySosoRange");
+var output_soso = document.getElementById("outputSoso");
+output_soso.innerHTML = slider_soso.value;
+
+slider_soso.oninput = function() {
+  output_soso.innerHTML = this.value;
+}
+
+var soso = d3.select("#mySosoRange").property("value");
+console.log(soso);
+
+// Bad Mood Input
+var slider_bad = document.getElementById("myBadRange");
+var output_bad = document.getElementById("outputBad");
+output_bad.innerHTML = slider_bad.value;
+
+slider_bad.oninput = function() {
+  output_bad.innerHTML = this.value;
+}
+
+var bad = d3.select("#myBadRange").property("value");
+console.log(bad);
+
+// Network function code
+function findnetwork() {
+//   d3.event.preventDefault();
+  var good = d3.select("#myGoodRange").property("value");
+  var fun = d3.select("#myFunRange").property("value");
+  var wow = d3.select("#myWowRange").property("value");
+  var sad = d3.select("#mySadRange").property("value");
+  var soso = d3.select("#mySosoRange").property("value");
+  var bad = d3.select("#myBadRange").property("value");
+  console.log(good);
+
+  var moodList = [good, fun, wow, sad, soso, bad];
+
+  d3.json("/get_your_recommendation", {
+      method: "POST",
+      body: JSON.stringify(
+          moodList
+      ),
+      headers: {
+          "Content-type": "application/json; charset=UTF-8"
+      }
+  }).then(function (moodresult, err) {
+      if (err) { throw err };
+      if (!moodresult) {
+          console.log("I wasn't able to get data from the Web API you selected.");
+          return;
+      }
+  })
+}
+
+// Reset Button clearForm function code
 function clearForm() {
-    d3.select("#myGoodRange").html("");
-    d3.select("#myFunRange").html("");
-    d3.select("#myWowRange").html("");
-    d3.select("#mySadRange").html("");
-    d3.select("#mySosoRange").html("");
-    d3.select("#myBadRange").html("");
-    d3.select("tbody").html("");
-
+  d3.select("#myGoodRange").html("")
+  d3.select("#outputGood").html("50")
+  d3.select("#myFunRange").html("")
+  d3.select("#outputFun").html("50")
+  d3.select("#myWowRange").html("")
+  d3.select("#outputWow").html("50")
+  d3.select("#mySosoRange").html("")
+  d3.select("#outputSoso").html("50")
+  d3.select("#mySadRange").html("")
+  d3.select("#outputSad").html("50")
+  d3.select("#myBadRange").html("")
+  d3.select("#outputBad").html("50")
+  document.getElementById("network").innerHTML=""
 }
